ef5996daf6fed4b01edebb992695773b968c6694
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2023-03-13 18:19:01 +0700

    feat: add register page

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import Home from './pages/Home/Home.js';
 import Datasets from './pages/Datasets/Datasets.js';
 import Organization from './pages/Organization/Organization.js';
 import Login from './pages/Login/Login.js';
+import Register from './pages/Register/Register.js';
 
 // import components
 import TheNavbar from './components/Navbar.js';
 
 function App() {
-  const excludeRoutes = ['/login']
+  const excludeRoutes = ['/login', '/register']
 
   return(
     <>
       {!excludeRoutes.includes(window.location.pathname) && <TheNavbar />}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='login' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='register' element={<Register />} />
         <Route path='/datasets' element={<Datasets />} />
         <Route path='/organization' element={<Organization />} />
       </Routes>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,7 +3,7 @@ import { Container, Form, Button, Card, Row, Col } from 'react-bootstrap';
 // import styles
 import '../../styles/login.css'
 
-export default function Login() {
+export default function Register(argument) {
 	return(
 		<Container className='vh-100 d-flex justify-content-center align-items-center'>
 			<div className='d-flex justify-content-center align-self-center'>
@@ -13,26 +13,38 @@ export default function Login() {
 				<Card style={{width: '64rem'}}>
 					<Card.Body className='d-flex justify-content-center align-items-center'>
 						<Form className='w-100'>
-							<h1>Login</h1>
+						 	<h1>Register</h1>
 							<Form.Group className="mb-3" controlId="formBasicEmail">
-				        <Form.Label>Username or Email address</Form.Label>
-				        <Form.Control type="text" placeholder="Enter email" />
+				        <Form.Label>Username</Form.Label>
+				        <Form.Control type="text" placeholder="JohnDoe" />
+				      </Form.Group>
+
+				      <Form.Group className="mb-3" controlId="formBasicEmail">
+				        <Form.Label>Full Name</Form.Label>
+				        <Form.Control type="text" placeholder="John Doe" />
+				      </Form.Group>
+
+				      <Form.Group className="mb-3" controlId="formBasicEmail">
+				        <Form.Label>Email</Form.Label>
+				        <Form.Control type="email" placeholder="johndoe@gmail.com" />
 				      </Form.Group>
 
 				      <Form.Group className="mb-3" controlId="formBasicPassword">
 				        <Form.Label>Password</Form.Label>
-				        <Form.Control type="password" placeholder="Password" />
+				        <Form.Control type="password" placeholder="********" />
 				      </Form.Group>
 
-				      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-				        <Form.Check type="checkbox" label="Remember Me" />
+				      <Form.Group className="mb-3" controlId="formBasicPassword">
+				        <Form.Label>Confirm Password</Form.Label>
+				        <Form.Control type="password" placeholder="********" />
 				      </Form.Group>
 
-				      <Button variant="primary" type="submit" className='w-100'>Login</Button>
+				      <Button variant="dark" type="submit" className='w-100'>Register</Button>
 
 				      <h2 className='text-center my-2'>OR</h2>
 
-				      <Button variant="dark" type="submit" className='w-100'>Register</Button>
+				      <Button variant="primary" type="submit" className='w-100'>Login</Button>
+				      
 						</Form>
 					</Card.Body>
 				</Card>
