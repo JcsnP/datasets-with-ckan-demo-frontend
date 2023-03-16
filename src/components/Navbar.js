2ae28231f77c305cba5c85d81939418ddd0f7a04
JcsnP <jcsnpch@gmail.com>
2023-03-17 06:39:43 +0700

    feat: add login system

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,47 @@
 import { Navbar, Nav, Container, Button, NavDropdown } from 'react-bootstrap';
 
 export default function TheNavbar() {
+
+	const logout = () => {
+		localStorage.clear();
+		window.location.href = '/';
+	}
+
 	return(
 		<Container className='my-4'>
 			<Navbar bg="light shadow-sm rounded" expand="lg">
 				<Container>
-					<Navbar.Brand href="#home">Datasets with CKAN</Navbar.Brand>
+					<Navbar.Brand href="/">Datasets with CKAN</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto w-100">
 							<Nav.Link href="/datasets">Datasets</Nav.Link>
 							<Nav.Link href="/organization">Organization</Nav.Link>
 							<Nav.Link href="/group">Group</Nav.Link>
-							<div className='d-flex w-100 justify-content-end'>
+							<div className='d-flex w-100 justify-content-end gap-2'>
 								<a href="login">
-									<Button variant='light'>Login</Button>
+									{
+										!localStorage.getItem('token') && (
+											<Button variant='light'>Login</Button>
+										)
+									}
+									{
+										localStorage.getItem('token') && (
+											<Button variant='light'>Profile</Button>
+										)
+									}
 								</a>
 								<a href="register">
-									<Button variant='dark'>Register</Button>
+									{
+										!localStorage.getItem('token') && (
+											<Button variant='dark'>Register</Button>
+										)
+									}
+									{
+										localStorage.getItem('token') && (
+											<Button variant='danger' onClick={() => {logout()}}>Logout</Button>
+										)
+									}
 								</a>
 							</div>
 						</Nav>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,36 @@
+import React, {useState, useEffect} from 'react';
 import { Container, Form, Button, Card, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 // import styles
 import '../../styles/login.css'
 
 export default function Login() {
 	document.title = 'Login';
+
+	const [name, setName] = useState('');
+	const [password, setPassword] = useState('');
+
+	const login = async() => {
+		const response = await axios.post(
+			'http://127.0.0.1:5001/ckanapi/v1/users/login',
+			{
+				name: name,
+				password: password
+			}
+		);
+		if(response.status === 200) {
+			console.log(response)
+			alert('success')
+
+			// set token
+			localStorage.setItem('token', response.data.token);
+
+			// redirect to homepage
+			window.location.href = '/';
+		}
+	}
+
 	return(
 		<Container className='vh-100 d-flex justify-content-center align-items-center'>
 			<div className='d-flex justify-content-center align-items-center'>
@@ -17,19 +43,19 @@ export default function Login() {
 							<h1>Login</h1>
 							<Form.Group className="mb-3" controlId="formBasicEmail">
 				        <Form.Label>Username or Email</Form.Label>
-				        <Form.Control type="text" placeholder="JohnDoe" />
+				        <Form.Control type="text" placeholder="JohnDoe" value={name} onChange={(e) => {setName(e.target.value)}} />
 				      </Form.Group>
 
 				      <Form.Group className="mb-3" controlId="formBasicPassword">
 				        <Form.Label>Password</Form.Label>
-				        <Form.Control type="password" placeholder="********" />
+				        <Form.Control type="password" placeholder="********" value={password} onChange={(e) => {setPassword(e.target.value)}} />
 				      </Form.Group>
 
 				      <Form.Group className="mb-3" controlId="formBasicCheckbox">
 				        <Form.Check type="checkbox" label="Remember Me" />
 				      </Form.Group>
 
-				      <Button variant="primary" type="submit" className='w-100'>Login</Button>
+				      <Button variant="primary" className='w-100' onClick={() => {login()}}>Login</Button>
 
 				      <h2 className='text-center my-2'>OR</h2>
 
